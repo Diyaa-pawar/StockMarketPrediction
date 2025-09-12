cf6a725ae4b584600f14234f74a2a8c71ef9f133
Diyaa-pawar <pawardiya14@gmail.com>
2025-09-12 09:05:50 +0530

    Add files via upload

---
+++ largestnumber.py
@@ -0,0 +1,16 @@
+#largest of three numbers
+num1 = int(input("Enter 1st numbers:"))
+num2 = int(input("Enter 2nd numbers:"))
+num3 = int(input("Enter 3rd numbers:"))
+ 
+if num1>num2:
+    if num1>num3:
+        print("num1 "num1" is greater." )
+
+if num2>num1:
+    if num2>num3:
+        print("num2 "num2" is greater." )
+
+if num3>num1:
+    if num3>num2:
+        print("num3 "num3" is greater." )
